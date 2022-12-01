9e6508cc56fdf835f8c573f63b249d4df624bd5f
Mahima Sharma <mahimasharma@Mahimas-Air.attlocal.net>
2022-11-30 20:56:54 -0500

    ques 4 updated

---
+++ Assignment_4/ques_1/app.py
@@ -0,0 +1,33 @@
+# Steps :-
+# 1.) Object recognition
+# 2.) Motion tracking of considered object
+# 3.) Dimention calculation of object -> real time
+
+import cv2;
+import numpy;
+from matplotlib import pyplot as plt
+  
+# Opening image
+img = cv2.imread("./object.png")
+
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+obj_data = cv2.CascadeClassifier('./data.xml')
+
+found = obj_data.detectMultiScale(img_gray, minSize =(20, 20))
+
+amount_found = len(found)
+
+if amount_found != 0:
+      
+    for (x, y, width, height) in found:
+
+        cv2.rectangle(img_rgb, (x, y), 
+                      (x + height, y + width), 
+                      (0, 255, 0), 5)
+          
+
+plt.subplot(1, 1, 1)
+plt.imshow(img_rgb)
+plt.show()
